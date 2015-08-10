8e56b1617141d4dadcbb2fe5834479052aedeca2
Spencer Roach <spencer.roach@twcable.com>
2015-08-10 00:58:33 -0400

    appointment creation almost complete
    need to debug association between appointment and patient.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -69,6 +69,6 @@ class PatientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def patient_params
-      params.require(:patient).permit(:name, :type, :breed, :age, :weight, :date_of_last_appointment)
+      params.require(:patient).permit(:name, :pet_type, :breed, :age, :weight, :date_of_last_appointment)
     end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ActiveRecord::Base
   has_many :patients
+  has_many :appointments, through: :patients
 end
