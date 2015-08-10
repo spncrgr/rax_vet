0556a6d37aee40e9e8909eded021853b4a96ad0a
Spencer Roach <spencer.roach@twcable.com>
2015-08-09 22:11:43 -0400

    basic scaffolding complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :encounters
+  resources :appointments
+  resources :patients
+  resources :users
+  resources :customers
+  resources :employees
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
