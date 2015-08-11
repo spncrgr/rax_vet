1f225662f88608fe35e6545d84037e916b67fa3c
Spencer Roach <spencer.roach@twcable.com>
2015-08-10 21:26:46 -0400

    only validate value of `years_in_practice` is employee is a physician

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,8 +1,18 @@
+class YearsValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if record.doctor
+      unless value > 0 || value <= 100
+        record.errors[attribute] << (options[:message] || 'is not between 1 and 100')
+      end
+    end
+  end
+end
 class Employee < ActiveRecord::Base
   has_many :appointments
   has_many :patients, through: :appointments
 
+
   validates :name, presence: true, length: { maximum: 35 }
   validates :zip_code, length: { maximum: 5 }
-  validates :years_in_practice, inclusion: {within: 0..100}
+  validates :years_in_practice, years: true
 end
